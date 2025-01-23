a9526dd15084c724d6feb5ab7fcb907eb9ea320b
Vydmintas <66644835+vydmintas@users.noreply.github.com>
2025-01-23 16:55:04 +0200

    Uploaded project files first time

---
+++ data/products.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: 'Knyga 1', price: 10 },
+    { id: 2, name: 'Knyga 2', price: 15 },
+    { id: 3, name: 'Knyga 3', price: 20 }
+];
+
+module.exports = products;
