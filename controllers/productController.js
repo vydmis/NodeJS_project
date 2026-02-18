99003b80ebed6bd42e4c6c924ddd4db778b1614d
Vydmintas <66644835+vydmintas@users.noreply.github.com>
2026-02-18 19:02:03 +0200

    test

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,45 @@
 const products = require('../data/products');
 
 const getAllProducts = (req, res) => {
-     return products;
+    res.json(products);
 };
 
 const getProductById = (req, res) => {
     const product = products.find(p => p.id === parseInt(req.params.id));
-    if (!product) return res.status(404).send('Produktas nerastas');
-    res.json(product);
+    if (!product) {
+        res.status(404).send('Produktas nerastas');
+        return null; // Grąžiname `null`, jei produktas nerastas
+    }
+    return product; // Grąžiname produktą
+};
+
+const updateProductById = (req, res) => {
+    const productIndex = products.findIndex(p => p.id === parseInt(req.params.id));
+    if (productIndex === -1) return res.status(404).send('Produktas nerastas');
+
+    const updatedProduct = { ...products[productIndex], ...req.body };
+    products[productIndex] = updatedProduct;
+    res.json(updatedProduct);
+};
+
+const deleteProductById = (req, res) => {
+    const productIndex = products.findIndex(p => p.id === parseInt(req.params.id));
+    if (productIndex === -1) return res.status(404).send('Produktas nerastas');
+
+    const deletedProduct = products.splice(productIndex, 1);
+    res.json(deletedProduct[0]);
+};
+
+const createProduct = (req, res) => {
+    const newProduct = req.body;
+    products.push(newProduct);
+    res.status(201).json(newProduct);
 };
 
 module.exports = {
     getAllProducts,
-    getProductById
+    getProductById,
+    updateProductById,
+    deleteProductById,
+    createProduct
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,25 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', async (req, res) => {
-    const products = productController.getAllProducts();
+    const products = await productController.getAllProducts();
     res.render('index', { products });
 });
 
+router.get('/new', (req, res) => {
+    res.render('product', { product: null });
+});
+
+router.get('/:id/edit', async (req, res) => {
+    const product = await productController.getProductById(req, res);
+    res.render('product', { product });
+});
+
 router.get('/:id', productController.getProductById);
+
+router.post('/', productController.createProduct);
+
+router.put('/:id', productController.updateProductById);
+
+router.delete('/:id', productController.deleteProductById);
 
 module.exports = router;
